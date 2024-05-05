ea3435ae48d69738cc6b0d136d31806761a1fb0f
hellojunho <jo62698476@gmail.com>
2024-05-05 19:55:24 +0900

    chore: modified serializers and urls

--- card/urls.py
+++ card/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("back/", views.get_card_back_image, name="card_back"),
     path("front/all/", views.get_card_front_info, name="card_front_all"),
-    path("answer/horoscope/", views.get_answer, name="get_answer"),
+    path("answer/horoscope/", views.get_answer_horoscope, name="get_answer_horoscope"),
+    path("answer/", views.get_answer, name="get_answer"),
 ]
