19defb83569899f0d248fc05e88cb4dfa54e3036
hellojunho <jo62698476@gmail.com>
2024-05-12 03:02:43 +0900

    refactor: [#9] Apply PEP8 code style and use serializer for gemini

--- gemini/views.py
+++ gemini/views.py
@@ -1,16 +1,22 @@
 import os
 import time
 import json
+from .logger import log
 
 import google.generativeai as genai
+from gemini.serializer import GoogleAIRequestSerializer, GoogleAIResponseSerializer
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from dotenv import load_dotenv
+
+
 load_dotenv()
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 GOOGLE_AI_TOKEN = os.getenv("GOOGLE_AI_TOKEN")
+
 
 @api_view(["POST", "GET"])
 def google_ai(request):
@@ -44,12 +50,16 @@ def google_ai(request):
     if request.method == "GET":
         return Response({"message": "This is a GET request"}, status=status.HTTP_200_OK)
 
+    # Confirm validation and deserialzing data by using serialized data model
+    serializer = GoogleAIRequestSerializer(data=request.data)
+    if not serializer.is_valid():
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     genai.configure(api_key=GOOGLE_API_KEY)
     model = genai.GenerativeModel('gemini-pro')
 
     content = ""
-    data = json.loads(request.body)
-    contents = data.get('contents', [])
+    contents = serializer.validated_data.get('contents', [])
 
     headers = {
         'Authorization': f'Token {GOOGLE_AI_TOKEN}',
@@ -57,6 +67,8 @@ def google_ai(request):
     }
 
     # parse user question in request form
+    # contents: user's request
+    # c: content of "contents" -> I use 'c' for parsing property that "contents/parts/text"
     for c in contents:
         parts = c.get('parts', [])
         for part in parts:
@@ -64,22 +76,20 @@ def google_ai(request):
             if text:
                 content = text
 
-
-    start = time.time()
     try:
+        start = time.time()
         response_text = model.generate_content(content)
         end = time.time()
-        print("Get response time: ", end - start)
+        log.info(f"Time taken to generate content {end - start}")
 
-        # Extract the required properties so you can serialize them to JSON
         candidates = response_text.candidates if response_text else []
         parts = [{'text': candidate.content.parts[0].text} for candidate in candidates]
 
         text = [part['text'] for part in parts]
-        # print("text: ", text)
 
-        return Response({"response" : text}, status=status.HTTP_200_OK)
+        # Deserialzing data by using serialized data model
+        response_serializer = GoogleAIResponseSerializer({'response': text})
+        return Response(response_serializer.data, status=status.HTTP_200_OK)
     except Exception as e:
         print(e)
-
-        return Response({"error":e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"error": e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
