7b0f6fffe394ea5a73efb75d995ab3ea1981a5f3
hellojunho <jo62698476@gmail.com>
2024-06-13 01:45:52 +0900

    feat: create AI Answer class in models.py

--- gemini/models.py
+++ gemini/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+from game.models import Game
+
+class AIAnswer(models.Model):
+    """
+    AI Answer Model
+    """
+    ai_answer_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    ai_answer = models.TextField(blank=True, null=False)
+    ai_answer_created_at = models.DateTimeField(auto_now_add=True)
+    ai_answer_of_game = models.ForeignKey(Game, on_delete=models.CASCADE, blank=False, null=False)
+
+    class Meta:
+        managed = True
+        db_table = 'ai_answer'
