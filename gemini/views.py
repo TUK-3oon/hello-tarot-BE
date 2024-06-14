4b720067e2586301c683e873bc1d1106eeea6070
hellojunho <jo62698476@gmail.com>
2024-06-15 02:07:25 +0900

    refactor: fix get gemini answer from gemini

--- gemini/views.py
+++ gemini/views.py
@@ -1,75 +1,81 @@
-# from gemini.logger import log
-import os
-import time
-from config.utils import validate_serializer, exception_handler, success_response
-from gemini.serializer import GoogleAIRequestSerializer, GoogleAIResponseSerializer
+import json
+from django.shortcuts import get_object_or_404
+from card.models import Card
+from config import settings
+from config.utils import get_text_answer, validate_serializer, exception_handler, success_response
+from game.models import Game
+from gemini.models import AIAnswer
+from gemini.serializer import GoogleAIRequestSerializer
 import google.generativeai as genai
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework import status
+from django.utils import timezone
 
-from dotenv import load_dotenv
-load_dotenv()
+GOOGLE_API_KEY = settings.GOOGLE_API_KEY
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-GOOGLE_AI_TOKEN = os.getenv("GOOGLE_AI_TOKEN")
-
-
-@api_view(["POST", "GET"])
+@api_view(["POST"])
 @exception_handler(view=True)
 def google_ai(request):
     """
     Use Gemini Prompt View
-     
-    Request:\n
+
+    Request:
         {
-            "contents":
-                [
-                    {"parts": [
-                        {"text":"python에 대해 설명해줘"}
+            "game_id": uuid of game,
+            "select_card_id": selected card id from user
+        }
+
+    Response:
+        {
+            "response": [
+                {
+                    "forward": [
+                        ...
+                    ],
+                    "reverse": [
+                        ...
                     ]
                 }
             ]
         }
-    
-
-    Response : \n
-        candidates {
-        index: 0
-            content {
-            parts {
-                text: "Python은 가장 많이 쓰이는 프로그래밍 언어입니다."
-            }
-            role: "model"
-            }
-        ...
-        }
     """
-    if request.method == "GET":
-        return success_response({"message": "This is a GET request"}, status=status.HTTP_200_OK)
-
     serializer = validate_serializer(GoogleAIRequestSerializer, request.data)
+
     genai.configure(api_key=GOOGLE_API_KEY)
     model = genai.GenerativeModel('gemini-pro')
-    content = ""
-    contents = serializer.get('contents', [])
-    headers = {
-        'Authorization': f'Token {GOOGLE_AI_TOKEN}',
-        'Content-Type': 'application/json',
-    }
 
-    for c in contents:
-        parts = c.get('parts', [])
-        for part in parts:
-            text = part.get('text', None)
-            if text:
-                content = text
+    game_id = serializer.get('game_id')
+    select_card_id = serializer.get('select_card_id')
+
+    game = Game.objects.get(pk=game_id)
+    game_type_name = game.game_type
+    game_question = game.game_quest
+
+    card = Card.objects.get(pk=select_card_id)
+    card_image_url = card.card_image_url
+
+    content = f"""
+    타로 게임을 시작할거야. 너는 세계에서 알아주는 타로술사야. 이번 게임의 주제는 '{game_type_name}'야.
+    너가 고객에게 한 질문은 '{game_question}'야.
+    고객이 고른 카드의 그림은 '{card_image_url}'야.
+    이 3가지의 내용을 조합해 적절한 운세 답변을 내줘.
+    긍정적인 답변과 부정적인 답변으로 나누어서 답변해주고, 각각의 상황에 앞으로 어떻게 행동하면 좋을지도 알려줘.
+    답변의 형식은 JSON이고, 긍정적인 답변의 키는 'forward', 부정적인 답변의 키는 'reverse'로 표시해줘.
+    하나의 key에 해당하는 값들은 리스트로 묶어서 표시하고, 무분별한 줄바꿈은 하지말고, 답변은 이쁘게 출력해.
+    그리고 답변은 코드 블럭으로 감싸지말고 JSON 형식에 맞게 출력해.
+    코드 블럭으로 절대 감싸지마.
+    """
 
     response_text = model.generate_content(content)
     candidates = response_text.candidates if response_text else []
-    parts = [{'text': candidate.content.parts[0].text} for candidate in candidates]
 
-    text = [part['text'] for part in parts]
+    response_data = get_text_answer(candidates)
 
-    response_data = GoogleAIResponseSerializer({'response': text})
-    return success_response(response_data.data, status=status.HTTP_200_OK)
+    AIAnswer.objects.create(
+        ai_answer=response_data,
+        ai_answer_created_at=timezone.now(),
+        ai_answer_of_game=game 
+    )
+
+    return success_response(json.dumps(response_data, ensure_ascii=False, indent=4), status.HTTP_200_OK)
+    # return success_response(response_data, status.HTTP_200_OK)
