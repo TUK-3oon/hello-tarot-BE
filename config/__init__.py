3e1ad2b14e75bde8f10ca9c4735bcec607d571f2
hellojunho <jo62698476@gmail.com>
2024-06-23 19:56:41 +0900

    refactor: refactored options about celery in config

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,4 @@
-# 이렇게 설정해주면 Django가 시작할떄 Celery가 항상 import 되고
-# shared_task 데코레이션이 Celery를 이용하게됩니다.
-from .celery import app as celery_app
+from __future__ import absolute_import, unicode_literals
+from config.celery import app as celery_app
 
 __all__ = ('celery_app',)
