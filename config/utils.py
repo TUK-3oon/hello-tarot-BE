880d6d7cf0199e7fbd365520470bc716be8b16cc
hellojunho <jo62698476@gmail.com>
2024-06-23 19:57:37 +0900

    refactor: add get_content, remove_special_characters in utils.py

--- config/utils.py
+++ config/utils.py
@@ -1,3 +1,4 @@
+import json
 import re
 from rest_framework import serializers
 
@@ -46,18 +47,19 @@ def exception_handler(view: bool = False):
                 return func(*args, **kwargs)
             except Card.DoesNotExist as e:
                 print(f"An error occurred in {func.__name__}: {e}")
-                return error_response("Invalid Card", status.HTTP_400_BAD_REQUEST)
+                return error_response("Invalid Card", status.HTTP_404_NOT_FOUND)
             except Game.DoesNotExist as e:
                 print(f"An error occuerd in {func.__name__}: {e}")
-                return error_response("Invalid Game", status.HTTP_400_BAD_REQUEST)
+                return error_response("Invalid Game", status.HTTP_404_NOT_FOUND)
             except Gametype.DoesNotExist as e:
                 print(f"An error occurred in {func.__name__}: {e}")
-                return error_response("Invalid Game Type", status.HTTP_400_BAD_REQUEST)
-            except AIAnswer.DoesNotExist as e:
-                print(f"An error occurred in {func.__name__}: {e}")
-                return error_response("Invalid AI Answer", status.HTTP_400_BAD_REQUEST)
+                return error_response("Invalid Game Type", status.HTTP_404_NOT_FOUND)
+            # except AIAnswer.DoesNotExist as e:
+            #     print(f"An error occurred in {func.__name__}: {e}")
+            #     return error_response("Invalid AI Answer", status.HTTP_404_NOT_FOUND)
             except Exception as e:
                 print(f"An error occurred in {func.__name__}: {e}")
+                print(e)
                 return error_response("Internal Server Error", status.HTTP_500_INTERNAL_SERVER_ERROR)
         return wrapper
     return exception_handler_decorator
@@ -66,5 +68,53 @@ def exception_handler(view: bool = False):
 def error_response(error: str, status_code: int):
     return Response({"error": error}, status_code)
     
-def success_response(data: dict, status_code: int):
-    return Response(data, status=status_code)
+
+def success_response(data, status_code: int):
+    return Response({"data": data}, status=status_code)
+
+
+def get_text_answer(response_text):
+    """
+    Get Answer from Gemini AI
+    """
+    candidates = response_text.candidates if response_text else []
+
+    text = []
+    for candidate in candidates:
+        if candidate.content and candidate.content.parts:
+            for part in candidate.content.parts:
+                text.append(part.text)
+
+    response_data = text
+
+    if isinstance(response_data, dict):
+        response = json.dumps(response_data, ensure_ascii=False)
+        return response
+    return response_data
+
+def remove_special_characters(text: str):
+    """
+    To remove '*', '\\', '\n' in text.
+    """
+    text = text.replace("\\", "")
+    text = text.replace("\n", "")
+    text = text.replace("*", "")
+    # text = text.replace("n", "")
+    
+    return text
+
+
+def get_content(response_text):
+    """
+    Get Content from Gemini AI
+    """
+        # candidates 리스트에서 text 추출
+    if "result" in response_text and "candidates" in response_text["result"]:
+        candidates = response_text["result"]["candidates"]
+        for candidate in candidates:
+            if "content" in candidate and "parts" in candidate["content"]:
+                parts = candidate["content"]["parts"]
+                for part in parts:
+                    if "text" in part:
+                        extracted_text = part["text"]
+                        return extracted_text
