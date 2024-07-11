0edb44b07f47c53dc49877e0bdc4a05dae26d631
hellojunho <jo62698476@gmail.com>
2024-07-11 14:39:44 +0900

    refactor: delete annotation in models.py

--- game/models.py
+++ game/models.py
@@ -6,7 +6,7 @@ class Game(models.Model):
     game_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     game_type = models.ForeignKey('Gametype', models.DO_NOTHING, blank=False, null=False)
     game_select_card_id = models.UUIDField(blank=False, null=True)
-    game_all_select_card_id = models.JSONField(blank=False, null=True)  # JSON 형식으로 변경
+    game_all_select_card_id = models.JSONField(blank=False, null=True)
     game_quest = models.TextField(blank=False, null=False)
     game_started_at = models.DateTimeField(blank=False, null=False)
     game_finished_at = models.DateTimeField(blank=False, null=True)
